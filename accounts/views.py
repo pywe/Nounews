7f42b9053eb69916a229307479c194fef848a42c
TheoElia <theophylusnhutiphapha@gmail.com>
2020-03-10 09:37:51 +0000

    index

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    template_name = "accounts/home2.html"
+    template_name = "accounts/index.html"
     args = {}
     return render(request,template_name,args)
